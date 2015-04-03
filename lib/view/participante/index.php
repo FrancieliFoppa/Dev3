30f3fe74cfd842217ca46a852072e0bc46d17677
Maiglon Alexandre Lubacheuski <maiglonl@gmail.com>
2015-04-02 22:47:04 -0300

    Padronizando index do Participante

--- lib/view/participante/index.php
+++ lib/view/participante/index.php
@@ -18,60 +18,60 @@
 		<![endif]-->
 	</head>
 	<body>
-	<div id="wrapper">
-		<!-- start header -->
-		<header>
-			<div class="navbar navbar-default navbar-static-top">
-				<div class="container">
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<a class="navbar-brand" href="index.html">DS<span>3</span></a>
-					</div>
-					
-					<div class="navbar-collapse collapse ">
-						<ul class="nav navbar-nav">
-							<li><a href="?show=site&action=home">Home</a></li>
-							<li class="dropdown ">
-								<a href="#" class="dropdown-toggle " data-toggle="dropdown" data-hover="dropdown" data-delay="0" data-close-others="false">teste DropDown <b class=" icon-angle-down"></b></a>
-								<ul class="dropdown-menu">
-									<li><a href="">t1</a></li>
-									<li><a href="">t2</a></li>
-									<li><a href="">t3 box</a></li>
-								</ul>
-							</li>
-							<li><a href="">menu item</a></li>
-							<li><a href="">menu item2</a></li>
-							<li class="active"><a href="">menu item 3</a></li>
-						</ul>
+		<div id="wrapper">
+			<!-- start header -->
+			<header>
+				<div class="navbar navbar-default navbar-static-top">
+					<div class="container">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="index.html">DS<span>3</span></a>
+						</div>
+						
+						<div class="navbar-collapse collapse ">
+							<ul class="nav navbar-nav">
+								<li><a href="?show=site&action=home">Home</a></li>
+								<li class="dropdown ">
+									<a href="#" class="dropdown-toggle " data-toggle="dropdown" data-hover="dropdown" data-delay="0" data-close-others="false">teste DropDown <b class=" icon-angle-down"></b></a>
+									<ul class="dropdown-menu">
+										<li><a href="">t1</a></li>
+										<li><a href="">t2</a></li>
+										<li><a href="">t3 box</a></li>
+									</ul>
+								</li>
+								<li><a href="">menu item</a></li>
+								<li><a href="">menu item2</a></li>
+								<li class="active"><a href="">menu item 3</a></li>
+							</ul>
+						</div>
 					</div>
 				</div>
-			</div>
-		</header>
-		<!-- end header -->
-		
-		<?php echo $view->show();	//Show central content ?>
-		
-		<footer>
-			<div id="sub-footer">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-6">
-							<div class="copyright">
-								<p><span> UNISINOS | Desenvolvimento de Software 3 - 2015/1 </span></p>
+			</header>
+			<!-- end header -->
+			
+			<?php echo $view->show();	//Show central content ?>
+			
+			<footer>
+				<div id="sub-footer">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-6">
+								<div class="copyright">
+									<p><span> UNISINOS | Desenvolvimento de Software 3 - 2015/1 </span></p>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</footer>
-	</div>
-	
-	<!-- javascript -->
-	<script src="js/jquery.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
+			</footer>
+		</div>
+		
+		<!-- javascript -->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
 	</body>
 </html>
